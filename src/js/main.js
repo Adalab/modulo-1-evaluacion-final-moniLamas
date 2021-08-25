1806202ad6379a737fc6e8dc8fd3a2283e043491
monipeny <mlamasrial@gmail.com>
2021-08-25 12:04:39 +0200

    add functions to navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const navbar = document.querySelector('.main-navbar');
+const toggle = document.querySelector('.toggle-btn');
+
+toggle.addEventListener('click', () => {
+    navbar
+        .classList
+        .toggle('is-open');
+    toggle
+        .querySelector('i')
+        .classList
+        .toggle('fa-times');
+} );
